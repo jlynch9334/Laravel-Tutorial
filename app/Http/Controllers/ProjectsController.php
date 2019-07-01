9ad97f4354f75341aac53bdf07d60d6e4a29628e
= <=>
2019-06-30 23:26:02 -0500

    Complete Service Container and Auto-Resolution Tutorial

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Project;
+use App\Services\Twitter;
 
 class ProjectsController extends Controller
 {
@@ -13,8 +14,10 @@ class ProjectsController extends Controller
        return view('projects.index', compact('projects'));
    }
 
-   public function show(Project $project)
+   public function show(Project $project, Twitter $twitter)
    {
+       dd($twitter);
+       
        return view('projects.show', compact('project'));
    }
 
