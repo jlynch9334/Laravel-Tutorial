a018acf0fbc874c1197b9e3a41a3ca5ac3c3c193
= <=>
2019-07-13 01:50:19 -0500

    Complete more tutorials

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -9,40 +9,6 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class ProjectPolicy
 {
     use HandlesAuthorization;
-    
-    /**
-     * Determine whether the user can view any projects.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Project  $project
-     * @return mixed
-     */
-    public function view(User $user, Project $project)
-    {
-        return $project->owner_id == $user->id;
-    }
-
-    /**
-     * Determine whether the user can create projects.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
 
     /**
      * Determine whether the user can update the project.
@@ -54,41 +20,5 @@ class ProjectPolicy
     public function update(User $user, Project $project)
     {
         return $project->owner_id == $user->id;
-    }
-
-    /**
-     * Determine whether the user can delete the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Project  $project
-     * @return mixed
-     */
-    public function delete(User $user, Project $project)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Project  $project
-     * @return mixed
-     */
-    public function restore(User $user, Project $project)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the project.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Project  $project
-     * @return mixed
-     */
-    public function forceDelete(User $user, Project $project)
-    {
-        //
     }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <h1 class="title">{{ $project->title }}</h1>)
+    <h1 class="title">{{ $project->title }}</h1>
 
     <div class="content">{{ $project->description }}
         <p>
